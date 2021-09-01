32ba49315b256dd7e5fcf72b1eab89405f76d178
BrunoMartinez17 <85202033+BrunoMartinez17@users.noreply.github.com>
2021-09-01 18:14:11 -0300

    Feat: home button

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './App.module.scss';
+import Boton from './components/Boton'
+
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>Hola</h1>
+      <Boton />
+
     </div>
   );
 }
---
+++ front-end/src/components/Boton.jsx
@@ -0,0 +1,19 @@
+import React, { Fragment, fragment } from 'react'
+
+import './Styles/Boton.css'
+
+
+const Boton = () => {
+    return (
+
+        <Fragment>
+            <div>
+                <button id="btn">
+                    Comenzar
+                </button>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Boton;
