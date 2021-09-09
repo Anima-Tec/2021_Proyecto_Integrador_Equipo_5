ab89f57f37d61cb833f614808210f69cf1282b92
bruno-dossantos <bruno.dossantos@globant.com>
2021-09-09 13:52:46 -0300

    creating login and install react-hoook-form

---
+++ front-end/src/static/login.js
@@ -0,0 +1,18 @@
+const content = {
+  inputs: [
+    {
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      register: 'email',
+    },
+    {
+      label: 'Password',
+      name: 'password',
+      type: 'password',
+      register: 'password',
+    },
+  ],
+};
+
+export default content;
