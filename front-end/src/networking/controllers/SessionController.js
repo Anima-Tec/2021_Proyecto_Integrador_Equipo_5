90caa0a193b48f63e62a7c9e34dddaf88c1c6cf7
Ezequiel Padilla <ezequiel.padilla.m@gmail.com>
2021-09-16 09:06:09 -0300

    Feat: login

--- front-end/src/networking/controllers/SessionController.js
+++ front-end/src/networking/controllers/SessionController.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { generateBackendURL } from '../routes';
 import TokenService from '../tokenService';
 
 class SessionController {
   static async login(email, password) {
-    const response = await axios.post(generateBackendURL('/auth/signin'), {
+    const response = await axios.post('http://127.0.0.1:8000/api/login/', {
       email,
       password,
     });
