4b851c9a47375a79a6ba9abe36ebd4400935e77a
bruno.dossantos <bruno.dossantos@globant.com>
2021-10-07 10:40:15 -0300

    css separated of html in home

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -9,8 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
-    <div className="container-fluid" style={{ width: '80%' }}>
-      <div className="row" style={{ height: '80vh' }}>
+    <div className={`container-fluid ${styles.container}`}>
+      <div className={`row ${styles.row}`}>
         <div className="col d-flex flex-column justify-content-center">
           <h1 className={styles.title}>¿Crees tener un buen estado nutricional?</h1>
           <label className={styles.body}>
@@ -18,9 +18,11 @@ export default function Home() {
             nutricional y descubrir como poder mejorar tu alimentacion diaria. Desde el equipo de
             KIWI te estaremos apoyando y ayudando durante todo el proceso.
           </label>
-          <button className={styles.button} type="button">
-            <Link className={styles.a} to="/test">REALIZAR TEST</Link>
-          </button>
+          <Link className={styles.btnContainer} to="/test">
+            <button className={styles.button} type="button">
+              REALIZAR TEST
+            </button>
+          </Link>
         </div>
         <div className="col d-none d-sm-flex d-sm-none d-md-flex d-flex  d-lg-flex justify-content-center align-items-center">
           <img src={Ilustration} alt="Ilustración" className={`animate__bounceIn ${styles.img}`} />
